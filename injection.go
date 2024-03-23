04d806a0e834603a30a9b1ee63927197ad146d42
lvyahui <lvyahui8@gmail.com>
2024-03-23 14:01:29 +0800

    实现bean定义和依赖图构建

--- injection.go
+++ injection.go
@@ -58,11 +58,26 @@ func parseTag(fieldDef reflect.StructField) (*AutowiredTags, *DIError) {
 	return res, nil
 }
 
-// AutoInject 手动触发图的依赖注入
-func AutoInject() {
+// AutoInject 手动触发完整图的依赖注入
+func AutoInject() *DIError {
 	// 对依赖图进行动态遍历（会不断有新的成员加入到图中）
+	var roots []beanName
+	// 迭代的过程中，g.nodes会不断有新的元素加入（一定是非root节点），所以分两次，先取出root
+	for name, node := range g.nodes {
+		if len(node.edgesIn) == 0 {
+			roots = append(roots, name)
+		}
+	}
+	for _, name := range roots {
+		diError := inject(g.nodes[name])
+		if diError != nil {
+			return diError
+		}
+	}
+	return nil
 }
 
+// inject 递归广度优先遍历依赖图，完成图中节点的依赖注入
 func inject(node *beanNode) *DIError {
 	// 遍历当前bean的所有成员字段，看哪些字段需要依赖注入
 	beanType := node.instance.beanType
@@ -90,5 +105,11 @@ func inject(node *beanNode) *DIError {
 		// 根据tag决策注入逻辑。
 	}
 	// 继续广度遍历子节点
+	for dependName := range node.edgesOut {
+		diError := inject(g.nodes[dependName])
+		if diError != nil {
+			return diError
+		}
+	}
 	return nil
 }
